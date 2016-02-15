45f5601c2e1dc262c1abe551fac2c8fdff14b416
Bo Yang <bonny95@gmail.com>
2016-02-14 22:54:50 -0800

    Create memleak_monitor.sh

---
+++ memleak_monitor.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+MAX_SIZE=20000000
+MON_FILE=/path/to/monitor_output_$(uname -n)
+while true
+do
+    date >> $MON_FILE
+    cat /proc/meminfo >> $MON_FILE
+    echo "----------------------" >> $MON_FILE
+    cat /proc/slabinfo >> $MON_FILE
+    echo "----------------------" >> $MON_FILE
+    ps -o pid,comm,stat,time,rss,vsz >> $MON_FILE
+    echo "++++++++++++++++++++++" >> $MON_FILE
+    fsize=`ls -l $MON_FILE | awk '{print $5}'`
+    if [ $fsize -gt $MAX_SIZE ]; then
+        suffix=`cat /proc/uptime | cut -d" " -f1`
+        mv $MON_FILE $MON_FILE.$suffix
+        #TODO: upload file to TFTP server or cloud
+    fi
+    sleep 3600 # 1 hour
+done
