233c9c79ac8bb864473e82510d0d5956003befc4
Bo Yang <bonny95@gmail.com>
2017-04-06 10:44:17 -0700

    Update memleak_plot.py

--- memleak_plot.py
+++ memleak_plot.py
@@ -2,8 +2,13 @@
 
 #
 # This is a simple script to plot memory usage/allocation trends based on the periodical snapshots of
-# /proc/meminfo and /proc/slabinfo. For more details, please refer to:
-# http://www.bo-yang.net/2015/03/30/debug-kernel-space-memory-leak
+# /proc/meminfo and /proc/slabinfo.
+#
+# Usage:
+#   memleak_plot.py -f <memory_monitor_output_file>
+#
+# For more details, please refer to:
+#   http://www.bo-yang.net/2015/03/30/debug-kernel-space-memory-leak
 #
 
 import os
@@ -31,8 +36,8 @@ def run_system(cmd):
         return None
 
 def draw_unreclaim(f):
-    LowFree = run_system("grep LowFree %s | awk '{print $2}'" %f)
-    LowFree = np.asarray(LowFree, dtype=int) / 1024 # MB
+    MemFree = run_system("grep MemFree %s | awk '{print $2}'" %f)
+    MemFree = np.asarray(MemFree, dtype=int) / 1024 # MB
     SUnreclaim = run_system("grep SUnreclaim %s | awk '{print $2}'" %f)
     SUnreclaim = np.asarray(SUnreclaim, dtype=int) / 1024 # MB
     x=np.arange(1, 2*len(SUnreclaim)+1, 2)
@@ -44,45 +49,49 @@ def draw_unreclaim(f):
     ax1.set_ylabel('SUnreclaim(MB)', color='r')
     for tl in ax1.get_yticklabels():
         tl.set_color('r')
-    #plt.legend()
+    plt.legend()
+    ax1.grid(True)
 
     ax2 = ax1.twinx() # draw two scales in one plot
-    ax2.plot(x, LowFree, 'b', label='LowFree')
-    ax2.set_ylabel('LowFree(MB)', color='b')
+    ax2.plot(x, MemFree, 'b', label='MemFree')
+    ax2.set_ylabel('MemFree(MB)', color='b')
     for tl in ax2.get_yticklabels():
         tl.set_color('b')
 
-    #plt.legend()
-    plt.show()
+    plt.legend()
+    #plt.show()
 
 def draw_kmalloc(f):
-    kmalloc8192 = run_system("grep kmalloc-8192 %s | awk '{print $2}'" %f)
+    kmalloc8192 = run_system("grep kmalloc-8192 %s | grep -v \"alloc_calls\"| awk '{print $2}'" %f)
     kmalloc8192 = np.asarray(kmalloc8192, dtype=int)
-    kmalloc4096 = run_system("grep kmalloc-4096 %s | awk '{print $2}'" %f)
+    kmalloc4096 = run_system("grep kmalloc-4096 %s | grep -v \"alloc_calls\"| awk '{print $2}'" %f)
     kmalloc4096 = np.asarray(kmalloc4096, dtype=int)
-    kmalloc2048 = run_system("grep kmalloc-2048 %s | awk '{print $2}'" %f)
+    kmalloc2048 = run_system("grep kmalloc-2048 %s | grep -v \"alloc_calls\"| awk '{print $2}'" %f)
     kmalloc2048 = np.asarray(kmalloc2048, dtype=int)
-    kmalloc1024 = run_system("grep kmalloc-1024 %s | awk '{print $2}'" %f)
+    kmalloc1024 = run_system("grep kmalloc-1024 %s | grep -v \"alloc_calls\"| awk '{print $2}'" %f)
     kmalloc1024 = np.asarray(kmalloc1024, dtype=int)
-    kmalloc512 = run_system("grep kmalloc-512 %s | awk '{print $2}'" %f)
+    kmalloc512 = run_system("grep kmalloc-512 %s | grep -v \"alloc_calls\"| awk '{print $2}'" %f)
     kmalloc512 = np.asarray(kmalloc512, dtype=int)
-    kmalloc256 = run_system("grep kmalloc-256 %s | awk '{print $2}'" %f)
+    kmalloc256 = run_system("grep kmalloc-256 %s | grep -v \"alloc_calls\"| awk '{print $2}'" %f)
     kmalloc256 = np.asarray(kmalloc256, dtype=int)
-    kmalloc128 = run_system("grep kmalloc-128 %s | awk '{print $2}'" %f)
+    kmalloc128 = run_system("grep kmalloc-128 %s | grep -v \"alloc_calls\"| awk '{print $2}'" %f)
     kmalloc128 = np.asarray(kmalloc128, dtype=int)
-    kmalloc64 = run_system("grep kmalloc-64 %s | awk '{print $2}'" %f)
+    kmalloc64 = run_system("grep kmalloc-64 %s | grep -v \"alloc_calls\"| awk '{print $2}'" %f)
     kmalloc64 = np.asarray(kmalloc64, dtype=int)
-    kmalloc192 = run_system("grep kmalloc-192 %s | awk '{print $2}'" %f)
+    kmalloc192 = run_system("grep kmalloc-192 %s | grep -v \"alloc_calls\"| awk '{print $2}'" %f)
     kmalloc192 = np.asarray(kmalloc192, dtype=int)
     x=np.arange(1, 2*len(kmalloc4096)+1, 2)
 
     fig,ax1 = plt.subplots()
-    ax1.plot(x, kmalloc4096, label="kmalloc-4096")
+    #ax1.plot(x, kmalloc4096, label="kmalloc-4096")
     ax1.plot(x, kmalloc256, label="kmalloc-256")
-    ax1.set_xlabel('uptime(hours)')
-    plt.legend()
+    ax1.plot(x, kmalloc128, label="kmalloc-128")
+    ax1.plot(x, kmalloc64, label="kmalloc-64")
+    ax1.set_xlabel('uptime')
+    ax1.grid(True)
+    plt.legend(loc='upper left')
 
-    plt.show()
+    #plt.show()
 
 def main(argv):
     """Main script entry point. """
@@ -97,6 +106,8 @@ def main(argv):
 
     draw_unreclaim(mon_file)
     draw_kmalloc(mon_file)
+
+    plt.show()
 
 
 if __name__ == '__main__':
