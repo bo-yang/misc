869431429e9a7d4a3ac0aa12f507d933bb8062da
Bo Yang <bonny95@gmail.com>
2018-01-12 10:18:59 -0800

    Create myalias.sh

---
+++ myalias.sh
@@ -0,0 +1,13 @@
+# Aliases defined in $HOME/.bashrc
+
+#Linux
+alias lf='ls -pa --color=auto'
+alias ll='ls -l --color=auto'
+alias save_terms='gnome-terminal --save-config=$HOME/gnome_tabs'
+alias load_terms='gnome-terminal --load-config=$HOME/gnome_tabs'
+
+#macOS
+alias lf='ls -aG'
+alias ll='ls -lG'
+alias gvim='/Applications/MacVim.app/Contents/MacOS/MacVim'
+alias gvimdiff='/Applications/MacVim.app/Contents/MacOS/MacVim'
